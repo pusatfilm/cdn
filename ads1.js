0215390edb160c9754ae904dfb7f6ca216cb75b4
pusatfilm <43159735+pusatfilm@users.noreply.github.com>
2019-06-10 15:53:43 +0800

    Create ads1.js

---
+++ ads1.js
@@ -0,0 +1,4 @@
+$(window).load(function(){
+document.getElementById('imageDiv')
+.innerHTML = '<img alt='ALT IMAGE' height='TINGGI IMAGE' src='URL IMAGE' title='TITLE IMAGE' width='LEBAR IMAGE'/>';
+});
