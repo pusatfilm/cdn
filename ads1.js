17242677769f6d66b894c6da425573492cff2fd5
pusatfilm <43159735+pusatfilm@users.noreply.github.com>
2019-06-10 15:55:12 +0800

    Update ads1.js

--- ads1.js
+++ ads1.js
@@ -1,4 +1,4 @@
 $(window).load(function(){
 document.getElementById('imageDiv')
-.innerHTML = '<img alt='ALT IMAGE' height='TINGGI IMAGE' src='https://cdns.klimg.com/dream.co.id/resized/640x320/news/2018/11/23/97044/hewan-yang-pertama-kali-kamu-lihat-di-gambar-ini-mampu-ungkap-kepribadianmu-181123b.jpg' title='TITLE IMAGE' width='LEBAR IMAGE'/>';
+.innerHTML = "<img alt='ALT IMAGE' height='TINGGI IMAGE' src='https://cdns.klimg.com/dream.co.id/resized/640x320/news/2018/11/23/97044/hewan-yang-pertama-kali-kamu-lihat-di-gambar-ini-mampu-ungkap-kepribadianmu-181123b.jpg' title='TITLE IMAGE' width='LEBAR IMAGE'/>";
 });
